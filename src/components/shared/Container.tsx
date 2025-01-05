309edf3eb3e6787bf6fbf35e3d63ca7fcd885b91
TheJohny71 <jduval71@me.com>
2025-01-04 19:53:49 -0600

    Update Container.tsx

--- src/components/shared/Container.tsx
+++ src/components/shared/Container.tsx
@@ -15,20 +15,20 @@ const maxWidthClasses = {
   lg: 'max-w-screen-lg',
   xl: 'max-w-screen-xl',
   '2xl': 'max-w-screen-2xl',
-  full: 'max-w-full',
+  full: 'max-w-full'
 };
 
 export function Container({
   children,
   className = '',
-  maxWidth = 'xl',
+  maxWidth = 'xl'
 }: ContainerProps) {
   return (
     <div
       className={cn(
         'mx-auto w-full px-4 sm:px-6 lg:px-8',
         maxWidthClasses[maxWidth],
-        className,
+        className
       )}
     >
       {children}
