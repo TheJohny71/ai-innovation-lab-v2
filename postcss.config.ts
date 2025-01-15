46e340432104dfb9d42145e36c0f7ab3ab9802d3
TheJohny71 <jduval71@me.com>
2025-01-15 21:11:48 +0000

    update

--- postcss.config.ts
+++ postcss.config.ts
@@ -2,9 +2,9 @@ import type { Config } from 'postcss-load-config';
 
 const config: Config = {
   plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
-  }
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
 export default config;
